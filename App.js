1fb20c339d852984e3582859b4aad0c47676aa03
Sascha <sascha.rose@gmail.com>
2023-08-07 14:35:37 +0200

    add MyEvent component

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+const events = [
+  {
+    title: "Fußball",
+    location: "Stadion",
+    numPlayers: 22,
+  },
+  {
+    title: "Volleyball",
+    location: "Halle",
+    numPlayers: 12,
+  },
+];
+
+function MyEvent({ title, location, numPlayers }) {
+  return (
+    <View style={{ backgroundColor: "lightblue" }}>
+      <Text style={{ fontWeight: "bold" }}>{title}</Text>
+      <Text>{location}</Text>
+      <Text>Anzahl der Spieler: {numPlayers}</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{events[0].title}</Text>
+      <Text>{events[0].location}</Text>
+      <Text>{events[0].numPlayers}</Text>
+      <Text>{events[1].title}</Text>
+      <Text>{events[1].location}</Text>
+      <Text>{events[1].numPlayers}</Text>
+
+      <MyEvent title="TestTitle" location="TestLocation" numPlayers={5} />
+      <MyEvent title="TestTitle" location="TestLocation" numPlayers={5} />
+      <MyEvent title="TestTitle" location="TestLocation" numPlayers={5} />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +46,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
