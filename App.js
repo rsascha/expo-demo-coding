9bdb777458e275214de360abd7ede3cffa20fa58
Sascha <sascha.rose@gmail.com>
2023-08-07 17:30:02 +0200

    add color

--- App.js
+++ App.js
@@ -34,11 +34,11 @@ const events = [
 
 // Eine Komponente, die dazu dient, ein einzelnes Event zu "rendern"
 
-function MyEvent({ title, location, numPlayers }) {
+function MyEvent({ title, location, numPlayers, backgroundColor }) {
   return (
     <View
       style={{
-        backgroundColor: "lightblue",
+        backgroundColor: backgroundColor,
         padding: 12,
         margin: 12,
         width: "100%",
@@ -57,6 +57,10 @@ function MyEvent({ title, location, numPlayers }) {
 export default function App() {
   const [counter, setCounter] = useState(0);
 
+  const maxNumPlayers = 10;
+
+  console.log("---------");
+
   return (
     <View style={styles.container}>
       <Text>{counter}</Text>
@@ -70,12 +74,24 @@ export default function App() {
       {/* Wir iterieren über unsere Events und erzeugen für jedes einzelne Event eine Instanz der MyEvent Komponente */}
       {events.map((event, index) => {
         console.log("Map Callback aufgerufen mit Event Nr.: " + index);
+        console.log(event);
+
+        let activeColor = "";
+
+        console.log(event.numPlayers < maxNumPlayers);
+
+        if (event.numPlayers < maxNumPlayers) {
+          activeColor = "lightblue";
+        } else {
+          activeColor = "orange";
+        }
         return (
           <MyEvent
             key={event.id}
             title={event.title}
             location={event.location}
             numPlayers={event.numPlayers}
+            backgroundColor={activeColor}
           />
         );
       })}
